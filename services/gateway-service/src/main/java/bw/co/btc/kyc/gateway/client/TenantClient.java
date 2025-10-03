5830ef5d96909414b1a03868a824d5c31b3182e7
Rodney Online <ronnyrodneynkhori@gmail.com>
2025-10-03 10:11:46 +0100

    biometric service

--- services/gateway-service/src/main/java/bw/co/btc/kyc/gateway/client/TenantClient.java
+++ services/gateway-service/src/main/java/bw/co/btc/kyc/gateway/client/TenantClient.java
@@ -1,5 +1,6 @@
 package bw.co.btc.kyc.gateway.client;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
